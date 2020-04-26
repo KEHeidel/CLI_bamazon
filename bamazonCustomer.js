9d56f1fcbb6a9e4ea0846ad3c2cbf20454c62055
Kortnie Heidel <kortnie.evans@gmail.com>
2020-04-26 16:08:09 -0400

    adds comments to code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,10 @@
+// npm packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var prompt = require("prompt");
 var Table = require("cli-table3")
 
+// connection for mysql
 var connection = mysql.createConnection({
     host: "localhost",
   
@@ -19,17 +21,16 @@ var connection = mysql.createConnection({
 
   connection.connect(function(err) {
       if (err) throw err;
-    //   console.log(`connected as id $(connection.threadId)`)
       showAllProducts();
   });
 
+  // function that shows products in the terminal in a table and then starts the function to make a purchase
   function showAllProducts() {
       var query = ("SELECT * FROM products");
       connection.query(query, function(err, res) {
           if (err) throw err;
         var table = new Table({
             head: ['Item ID', 'Product', 'Department', 'Price', 'Number In Stock'],
-            // colWidths: [10, 30, 15, 10, 10, 15]
           });
           for (var i = 0; i < res.length; i++) {
             table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity]);
@@ -37,12 +38,13 @@ var connection = mysql.createConnection({
           console.log(table.toString() + "\n");
           makePurchase();
         });
-        // connection.end();
 }
   
+// function to make a purchase of one of the products listed in the table
 function makePurchase() {
   connection.query("SELECT * FROM products", function(err, results) {
     if (err) throw err;
+  // prompt that asks the user which item they would like to purchase
   inquirer
     .prompt([
       {
@@ -58,11 +60,13 @@ function makePurchase() {
       }
     },
     {
+      // prompt that asks the user how many of the selected item they would like to purchase
       name: "number",
       type: "input",
       message: "How many items would you like to purchase?"
     }
   ])
+  // finds the selected product
   .then(function(answer) {
     var chosenItem;
     for (var i = 0; i < results.length; i++) {
@@ -71,6 +75,8 @@ function makePurchase() {
       }
     }
 
+    // checks to make sure that there is enough of the selected item in stock. if there is then the stock
+    // will be depleted by the total number being purchased.
     if (chosenItem.stock_quantity > parseInt(answer.number)) {
       var newQuantity = parseInt(chosenItem.stock_quantity - answer.number)
       connection.query(
@@ -83,6 +89,7 @@ function makePurchase() {
             item_id: chosenItem.item_id
           }
         ],
+        // lists the total price of the purchase
         function(error) {
           if (error) throw err;
           console.log("Total cost: ", chosenItem.price * answer.number);
@@ -90,12 +97,12 @@ function makePurchase() {
         }
       );
     }
+    // if there is not enough of the item in the stock then "insufficient inventory" will be
+    // displayed and the function to showallproducts will run.
     else {
-      // bid wasn't high enough, so apologize and start over
       console.log("Insufficient inventory");
       showAllProducts();
     }
   })
-    // connection.end();
 });
 }
