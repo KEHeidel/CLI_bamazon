0f23dacfc613d6e67fdd4b9d7347cb79e1e5b676
Kortnie Heidel <kortnie.evans@gmail.com>
2020-10-15 12:36:53 -0400

    adds bamazonManager.js and sql connection

---
+++ bamazonManager.js
@@ -0,0 +1,27 @@
+// npm packages
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var prompt = require("prompt");
+var Table = require("cli-table3")
+
+// connection for mysql
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Shiloh_519!*",
+    database: "bamazon_DB"
+  });
+
+  connection.connect(function(err) {
+      if (err) throw err;
+      showAllProducts();
+  });
+
+  
